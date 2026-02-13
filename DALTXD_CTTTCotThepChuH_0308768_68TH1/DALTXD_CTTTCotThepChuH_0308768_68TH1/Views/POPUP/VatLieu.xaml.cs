14c7f5e76808b5b528af0ce830440c3c411f8fff
kienlt0306568 <ltkien103@gmail.com>
2026-02-13 10:32:03 +0700

    tạo popup vật liệu

--- DALTXD_CTTTCotThepChuH_0308768_68TH1/DALTXD_CTTTCotThepChuH_0308768_68TH1/Views/POPUP/VatLieu.xaml.cs
+++ DALTXD_CTTTCotThepChuH_0308768_68TH1/DALTXD_CTTTCotThepChuH_0308768_68TH1/Views/POPUP/VatLieu.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using System.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -11,12 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
 namespace DALTXD_CTTTCotThepChuH_0308768_68TH1.Views.POPUP
 {
-    /// <summary>
-    /// Interaction logic for VatLieu.xaml
-    /// </summary>
     public partial class VatLieu : Window
     {
         public VatLieu()
--- DALTXD_CTTTCotThepChuH_0308768_68TH1/DALTXD_CTTTCotThepChuH_0308768_68TH1/Views/Ribbon.xaml.cs
+++ DALTXD_CTTTCotThepChuH_0308768_68TH1/DALTXD_CTTTCotThepChuH_0308768_68TH1/Views/Ribbon.xaml.cs
@@ -1,5 +1,4 @@
-﻿using DALTXD_CTTTCotThepChuH_0308768_68TH1.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using DALTXD_CTTTCotThepChuH_0308768_68TH1.Views.POPUP;
 namespace DALTXD_CTTTCotThepChuH_0308768_68TH1.Views
 {
     /// <summary>
@@ -21,9 +20,29 @@ namespace DALTXD_CTTTCotThepChuH_0308768_68TH1.Views
     /// </summary>
     public partial class Ribbon : UserControl
     {
+        public Window Owner { get; private set; }
+
         public Ribbon()
         {
             InitializeComponent();
+        }
+        private void BtnVatLieu_Click(object sender, RoutedEventArgs e)
+        {
+            VatLieu w = new VatLieu();
+            w.Owner = Window.GetWindow(this);
+            w.ShowDialog();
+        }
+        private void BtnthongsoDauvao_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new thongsodauvao();
+            win.Owner = Application.Current.MainWindow;
+            win.ShowDialog();
+        }
+        private void Btnthongsothietke_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new thongsothietke();
+            win.Owner = Application.Current.MainWindow;
+            win.ShowDialog();
         }
     }
 }
